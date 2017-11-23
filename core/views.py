edcdb5f99de13681b29116f18330eba1076aea3e
wellingtonmartins <wellingtonmartins77@hotmail.com>
2017-11-23 08:03:35 -0300

    Meu Primeiro git

---
+++ core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from  django.http import HttpResponse 
+
+# Create your views here.
+def index(request):
+    return render( request,'index.html')
+
+def contato(request):
+    return render( request,'contato.html')
+
+def produto(request):
+    return render( request,'produto.html')
+
+def produtos(request):
+    return render( request,'produtos.html')
+
